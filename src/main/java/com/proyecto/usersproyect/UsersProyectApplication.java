e7bd8edcfe504e5af108a020128b89c432e22434
Ciro Schot <cirschot@gmail.com>
2026-02-01 17:31:13 -0300

    Initial commit

---
+++ src/main/java/com/proyecto/usersproyect/UsersProyectApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.usersproyect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersProyectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersProyectApplication.class, args);
+	}
+
+}
