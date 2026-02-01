43b54cfc12d6032c589f2232381eaf046da72510
Ciro Schot <cirschot@gmail.com>
2026-02-01 20:19:30 -0300

    Create UserNotFoundException.java

---
+++ src/main/java/com/proyecto/usersproyect/exception/UserNotFoundException.java
@@ -0,0 +1,9 @@
+package com.proyecto.usersproyect.exception;
+
+public class UserNotFoundException extends RuntimeException
+{
+    public UserNotFoundException(Integer id)
+    {
+        super("User not found with id: " + id);
+    }
+}
