22b4b8767768d123035c6bedbd77e99a4a557389
Ciro Schot <cirschot@gmail.com>
2026-02-01 19:47:54 -0300

    User Service created with his functions

--- src/main/java/com/proyecto/usersproyect/service/UserService.java
+++ src/main/java/com/proyecto/usersproyect/service/UserService.java
@@ -1,5 +1,18 @@
 package com.proyecto.usersproyect.service;
 
-public class UserService
+import com.proyecto.usersproyect.model.User;
+
+import java.util.List;
+
+public interface UserService
 {
+    User create(User user);
+
+    List<User> getAll();
+
+    User getById(int id);
+
+    User update(int id, User user);
+
+    Void delete(int id);
 }
