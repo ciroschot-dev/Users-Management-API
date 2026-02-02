a46f48eca42b5970c4d0e5d862ef9b039e45aa62
Ciro Schot <cirschot@gmail.com>
2026-02-01 22:28:10 -0300

    Implementation of the CRUD on the UserController

--- src/main/java/com/proyecto/usersproyect/controller/UserController.java
+++ src/main/java/com/proyecto/usersproyect/controller/UserController.java
@@ -1,9 +1,49 @@
 package com.proyecto.usersproyect.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.proyecto.usersproyect.model.User;
+import com.proyecto.usersproyect.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-@RestController("/user")
+import java.util.List;
+
+@RequiredArgsConstructor
+@RequestMapping("/user")
+@RestController
+
 public class UserController
 {
+    private final UserService userService;
 
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user)
+    {
+        return ResponseEntity.ok(userService.save(user));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers()
+    {
+        return ResponseEntity.ok(userService.getAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable int id)
+    {
+        return ResponseEntity.ok(userService.findById(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUserById(@PathVariable int id, @RequestBody User updatedUser)
+    {
+        return ResponseEntity.ok(userService.update(id, updatedUser));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUserById(@PathVariable int id)
+    {
+        userService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
