ca62fe6cc650e769202444f7fbb322e679d33825
Ciro Schot <cirschot@gmail.com>
2026-03-22 11:40:59 -0300

    Rename project to users_management and update version to 1.0

--- src/main/java/com/proyecto/usersproyect/UsersProyectApplication.java
+++ src/main/java/com/proyecto/usersproyect/UsersProyectApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class UsersProyectApplication
 {
 
-    static void main(String[] args)
+    public static void main(String[] args)
     {
         SpringApplication.run(UsersProyectApplication.class, args);
     }
